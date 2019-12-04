2dabfb30d156547bd0dd936ecdde49a7638711a5
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-12-04 21:37:16 +0100

    interfaces swap as dependiencies

--- src/AppCore/Domain/Service/Command/WebSocketAdapter/OutputAdapterFactory.php
+++ src/AppCore/Domain/Service/Command/WebSocketAdapter/OutputAdapterFactory.php
@@ -4,27 +4,29 @@ declare(strict_types=1);
 namespace App\AppCore\Domain\Service\Command\WebSocketAdapter;
 
 use App\AppCore\Domain\Service\Command\CommandProcessor;
+use App\AppCore\Domain\Service\Command\OutputAdapterInterface;
 
 class OutputAdapterFactory implements OutputAdapterFactoryInterface
 {
     /**
-     * @var SocketProgressBarOutputAdapter
+     * @var OutputAdapterInterface
      */
     private $socketProgressBarOutputAdapter;
 
     /**
-     * @var SocketErrorOutputAdapter
+     * @var OutputAdapterInterface
      */
     private $socketErrorOutputAdapter;
 
     /**
      * OutPutFactory constructor.
-     * @param SocketProgressBarOutputAdapter $socketProgressBarOutputAdapter
-     * @param SocketErrorOutputAdapter $socketErrorOutputAdapter
+     *
+     * @param OutputAdapterInterface $socketProgressBarOutputAdapter
+     * @param OutputAdapterInterface $socketErrorOutputAdapter
      */
     public function __construct(
-        SocketProgressBarOutputAdapter $socketProgressBarOutputAdapter,
-        SocketErrorOutputAdapter $socketErrorOutputAdapter
+        OutputAdapterInterface  $socketProgressBarOutputAdapter,
+        OutputAdapterInterface $socketErrorOutputAdapter
     ) {
         $this->socketProgressBarOutputAdapter = $socketProgressBarOutputAdapter;
         $this->socketErrorOutputAdapter = $socketErrorOutputAdapter;
