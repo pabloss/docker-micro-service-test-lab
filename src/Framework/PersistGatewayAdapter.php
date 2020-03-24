2184d1d990d89215015ee7524185cf5f159273d5
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-03-24 19:51:00 +0100

    Persist Gateway Adapter

---
+++ src/Framework/PersistGatewayAdapter.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Framework;
+
+use App\AppCore\Domain\Repository\EntityInterface;
+use App\AppCore\Domain\Repository\PersistGatewayInterface;
+use App\Entity\UService;
+use App\Repository\UServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PersistGatewayAdapter implements PersistGatewayInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function nextId()
+    {
+        return $this->entityManager->getRepository(UService::class)->count([])+1;
+    }
+
+    public function getAll()
+    {
+        return $this->entityManager->getRepository(UService::class)->findAll();
+    }
+
+    public function persist(EntityInterface $uServiceEntity)
+    {
+        $this->entityManager->persist($uServiceEntity);
+        $this->entityManager->flush();
+    }
+
+}
---
+++ tests/unit/PersistGatewayAdapterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Entity\UService;
+use App\Framework\PersistGatewayAdapter;
+use App\Repository\UServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PersistGatewayAdapterTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testNextId()
+    {
+        $intiCount = 0;
+        $repo = $this->prophesize(UServiceRepository::class);
+        $repo->count([])->willReturn($intiCount);
+        $em = $this->prophesize(EntityManagerInterface::class);
+        $em->getRepository(UService::class)->willReturn($repo);
+
+        $adapter = new PersistGatewayAdapter($em->reveal());
+        $this->tester->assertEquals(1, $adapter->nextId());
+    }
+
+    public function testPersist()
+    {
+        $entity= new UService();
+        $em = $this->prophesize(EntityManagerInterface::class);
+        $adapter = new PersistGatewayAdapter($em->reveal());
+        $em->persist($entity)->shouldBeCalled();
+        $em->flush()->shouldBeCalled();
+
+        $adapter->persist($entity);
+    }
+
+    public function testGetAll()
+    {
+
+        $repo = $this->prophesize(UServiceRepository::class);
+        $repo->findAll()->willReturn([]);
+        $em = $this->prophesize(EntityManagerInterface::class);
+        $em->getRepository(UService::class)->willReturn($repo);
+
+        $adapter = new PersistGatewayAdapter($em->reveal());
+        $this->tester->assertEquals([], $adapter->getAll());
+    }
+}
