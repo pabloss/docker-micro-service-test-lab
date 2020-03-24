004eb5a58bdffc815981c55d7e97860ade162716
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-03-24 21:01:22 +0100

    UF Adapter

--- src/Framework/Files/UploadedFileAdapter.php
+++ src/Framework/Files/UploadedFileAdapter.php
@@ -3,7 +3,30 @@ declare(strict_types=1);
 
 namespace App\Framework\Files;
 
-class UploadedFileAdapter
-{
+use App\AppCore\Application\UploadedFileFactoryInterface;
+use App\AppCore\Domain\Actors\UploadedFileInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
+class UploadedFileAdapter implements UploadedFileInterface
+{
+    /**
+     * @var UploadedFile
+     */
+    private $frameworkUploadedFile;
+
+    /**
+     * UploadedFileAdapter constructor.
+     *
+     * @param UploadedFile $frameworkUploadedFile
+     */
+    public function __construct(UploadedFile $frameworkUploadedFile)
+    {
+        $this->frameworkUploadedFile = $frameworkUploadedFile;
+    }
+
+
+    public function getBasename(): string
+    {
+        return $this->frameworkUploadedFile->getBasename();
+    }
 }
---
+++ tests/unit/UploadedFileAdapterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Framework\Files\UploadedFileAdapter;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploadedFileAdapterTest extends \Codeception\Test\Unit
+{
+
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testGetBasename()
+    {
+        // mam wykorzystać framework - czyli jednak integracyjny
+        $path = __DIR__ . '/../_data/save_test'; //do integracyjhnego
+        $originalFileName = basename($path);
+
+        $adapter = new UploadedFileAdapter(new UploadedFile($path, $originalFileName));
+
+        $this->tester->assertEquals($originalFileName, $adapter->getBasename());
+    }
+}
