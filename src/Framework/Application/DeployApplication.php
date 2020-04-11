b0be7a88fb28cd9b505f56adacc83568abe08ae5
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-11 12:01:31 +0200

    UnpackImpl

---
+++ src/Framework/Application/DeployApplication.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Framework\Application;
+
+class DeployApplication
+{
+
+    /**
+     * DeployApplication constructor.
+     *
+     * @param UnpackService $service
+     */
+    public function __construct(\App\AppCore\Domain\Service\UnpackService $service)
+    {}}
