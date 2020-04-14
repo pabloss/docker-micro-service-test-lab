8629e773ac109dbb91c884e2a659b93867f02542
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-14 22:28:19 +0200

    test

--- tests/unit/Integration/App/AppCore/Application/DeployApplicationTest.php
+++ tests/unit/Integration/App/AppCore/Application/DeployApplicationTest.php
@@ -1,10 +1,12 @@
 <?php
 
+namespace Integration\App\AppCore\Application;
+
+use App\AppCore\Application\DeployApplication;
 use App\AppCore\Domain\Actors\uService;
 use App\AppCore\Domain\Repository\DomainEntityMapper;
 use App\AppCore\Domain\Repository\uServiceRepository;
 use App\AppCore\Domain\Service\UnpackService;
-use App\Framework\Application\DeployApplication;
 use App\Framework\Service\UnpackAdapter;
 use Codeception\Util\Autoload;
 use Integration\Stubs\PersistGateway;
@@ -12,7 +14,7 @@ use Integration\Stubs\PersistGateway;
 class DeployApplicationTest extends \Codeception\Test\Unit
 {
     const TEST_UPLOADED_DIR = "/../../files/";
-    const DATA_DIR =   __DIR__.'/../_data/';
+    const DATA_DIR = __DIR__ . '/../_data/';
     const TEST_UPLOAD_FILE_NAME = 'test.upload';
     const ZIPPED_TEST_UPLOAD_FILE_NAME = 'test.zip';
     const UNPACKED = 'unpacked';
@@ -25,7 +27,7 @@ class DeployApplicationTest extends \Codeception\Test\Unit
 
     protected function _before()
     {
-        Autoload::addNamespace('Integration\Stubs', __DIR__.'/Integration/Stubs/');
+        Autoload::addNamespace('Integration\Stubs', __DIR__ . '/../../../../Integration/Stubs/');
     }
 
     protected function _after()
@@ -40,6 +42,7 @@ class DeployApplicationTest extends \Codeception\Test\Unit
          *      - stosując zależność UnpackServiceInterface oraz Repo: rozpakuj (do nowej lokalizcji) i zapisz do bazy
          * 2. robić build
          */
+        $id = 'id';
         // Given
         $repo = new uServiceRepository(new PersistGateway(), new DomainEntityMapper());
         $application = new DeployApplication(
@@ -48,11 +51,11 @@ class DeployApplicationTest extends \Codeception\Test\Unit
         ); //Framework
 
         // When
-        $repo->persist(new uService(self::DATA_DIR.self::ZIPPED_TEST_UPLOAD_FILE_NAME, self::DATA_DIR));
-        $application->deploy('id', self::DATA_DIR.self::UNPACKED);
+        $repo->persist(new uService(self::DATA_DIR . self::ZIPPED_TEST_UPLOAD_FILE_NAME, self::DATA_DIR));
+        $application->deploy($id, self::DATA_DIR . self::UNPACKED . $id);
 
         // Then
-        $this->tester->assertFileExists(self::DATA_DIR. self::UNPACKED .'/'.self::TEST_UPLOAD_FILE_NAME);
-        $this->tester->assertEquals(self::DATA_DIR. self::UNPACKED, $repo->find('id')->unpacked());
+        $this->tester->assertFileExists(self::DATA_DIR . self::UNPACKED . $id . '/' . self::TEST_UPLOAD_FILE_NAME);
+        $this->tester->assertEquals(self::DATA_DIR . self::UNPACKED . $id, $repo->find($id)->unpacked());
     }
 }
