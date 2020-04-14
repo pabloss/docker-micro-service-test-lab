84050a98a7765423650407e02a64a7a46f2e912c
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-14 22:19:39 +0200

    test

--- src/AppCore/Domain/Service/UnpackServiceInterface.php
+++ src/AppCore/Domain/Service/UnpackServiceInterface.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\AppCore\Domain\Service;
 
-class UnpackServiceInterface
-{
+use App\AppCore\Domain\Actors\uServiceInterface;
 
+interface UnpackServiceInterface
+{
+    public function unpack(uServiceInterface $service, string $unpackedLocation);
 }
