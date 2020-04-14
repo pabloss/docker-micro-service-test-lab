eacd465f9e8fbaf58e3d3ccd9a3f8e57b237de9a
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-14 23:01:46 +0200

    test

--- tests/unit/App/AppCore/Domain/Service/UnpackServiceTest.php
+++ tests/unit/App/AppCore/Domain/Service/UnpackServiceTest.php
@@ -1,9 +1,8 @@
 <?php
 
+namespace App\AppCore\Domain\Service;
+
 use App\AppCore\Domain\Actors\uServiceInterface;
-use App\AppCore\Domain\Service\UnpackInterface;
-use App\AppCore\Domain\Service\UnpackService;
-use App\AppCore\Domain\Service\UnpackServiceInterface;
 
 class UnpackServiceTest extends \Codeception\Test\Unit
 {
@@ -11,7 +10,7 @@ class UnpackServiceTest extends \Codeception\Test\Unit
      * @var \UnitTester
      */
     protected $tester;
-    
+
     // tests
     public function testSomeFeature()
     {
@@ -23,17 +22,18 @@ class UnpackServiceTest extends \Codeception\Test\Unit
         $uService = $this->prophesize(uServiceInterface::class);
         $uService->file()->willReturn($file);
         $uService->movedToDir()->willReturn($movedToDir);
-        $uService->setUnpacked($unpackedDir.$id)->will(function ($args) use ($uService, $unpackedDir, $id){
-            $uService->unpacked()->willReturn($unpackedDir.$id);
+        $uService->setUnpacked($unpackedDir . $id)->will(function ($args) use ($uService, $unpackedDir, $id) {
+            $uService->unpacked()->willReturn($unpackedDir . $id);
         });
 
         $unpackLibAdapter = $this->prophesize(UnpackInterface::class);
-        $unpackLibAdapter->unpack($uService->reveal()->movedToDir().'/'.$uService->reveal()->file(), $unpackedDir.$id)->shouldBeCalled();
+        $unpackLibAdapter->unpack($uService->reveal()->file(),
+            $unpackedDir . $id)->shouldBeCalled();
 
         $service = new UnpackService($unpackLibAdapter->reveal());
-        $updatedUService = $service->unpack($uService->reveal(), $unpackedDir.$id);
+        $updatedUService = $service->unpack($uService->reveal(), $unpackedDir . $id);
 
         $this->tester->assertInstanceOf(UnpackServiceInterface::class, $service);
-        $this->tester->assertStringStartsWith($unpackedDir.$id, $updatedUService->unpacked());
+        $this->tester->assertStringStartsWith($unpackedDir . $id, $updatedUService->unpacked());
     }
 }
