fcdbb6f057b2e9d6b91df6dfce8024f652cbaa6e
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-24 14:35:37 +0200

    CommandRunner run impl change

--- tests/unit/Integration/App/Framework/Command/Stubs/RunnerStub.php
+++ tests/unit/Integration/App/Framework/Command/Stubs/RunnerStub.php
@@ -3,7 +3,26 @@ declare(strict_types=1);
 
 namespace App\Tests\unit\Integration\App\Framework\Command\Stubs;
 
-class RunnerStub
-{
+use App\AppCore\Domain\Service\CommandInterface;
+use App\AppCore\Domain\Service\CommandsCollectionInterface;
+use App\Framework\Service\Command\CommandRunner;
 
+class RunnerStub extends CommandRunner
+{
+    public static $counter = 0;
+
+
+    public function run(CommandsCollectionInterface $collection)
+    {
+
+        for($i = 0; $i < $collection->count(); $i++){
+            $this->runOneCommand($collection->getCommand($i));
+            ++self::$counter;
+        }
+    }
+
+    private function runOneCommand(CommandInterface $command)
+    {
+        $command->command();
+    }
 }
