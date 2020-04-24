6cb4697cd2b576b29b26844348ea7048ca194173
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-24 15:19:05 +0200

    CommandRunner run impl change

--- tests/unit/Integration/App/AppCore/Domain/Service/BuildServiceTest.php
+++ tests/unit/Integration/App/AppCore/Domain/Service/BuildServiceTest.php
@@ -1,16 +1,20 @@
 <?php namespace Integration\App\AppCore\Domain\Service;
 
 use App\AppCore\Domain\Service\BuildService;
+use App\Framework\Service\Command\BuildCommand;
+use App\Framework\Service\Command\CommandCollection;
+use App\Framework\Service\Command\CommandRunner;
+use App\Framework\Service\Command\RunCommand;
 
 class BuildServiceTest extends \Codeception\Test\Unit
 {
     const TEST_UPLOADED_DIR = "/../../files/";
-    const DATA_DIR = __DIR__ . '/../../../../../_data/';
+    const DATA_DIR = __DIR__ . '/../../../../../../_data/';
     const TEST_UPLOAD_FILE_NAME = 'test.upload';
     const ZIPPED_TEST_UPLOAD_FILE_NAME = 'test.zip';
     const UNPACKED = 'unpacked';
     const PACKED_MICRO_SERVICE = 'packed/micro-service-1.zip';
-    const MICRO_SERVICE_1_DOCKERFILE = 'micro-service-1/Dockerfile';
+    const MICRO_SERVICE_1_DOCKERFILE = 'scratch/Dockerfile';
 
     /**
      * @var \UnitTester
@@ -28,16 +32,38 @@ class BuildServiceTest extends \Codeception\Test\Unit
     // tests
     public function testSomeFeature()
     {
-        $uuid = \uniqid();
-        $imageName = 'test_image'.'_'.$uuid;
-        $containerName = 'test_container'.'_'.$uuid;
-        $this->tester->runShellCommand(" docker rmi $(docker images '$imageName*' --format '{{.ID}}') -f", false);
-        $this->tester->runShellCommand("docker rm  $(docker ps --filter 'name=$containerName' --format '{{.ID}}')", false);
-
         $id = 'id';
-        $service = new BuildService();
+        $uuid = \uniqid();
+
+        $imagePrefix = 'test_image';
+        $containerPrefix = 'test_container';
+
+        $imageName = $imagePrefix .'_'.$uuid;
+        $containerName = $containerPrefix .'_'.$uuid;
+
+        $data = [];
+        exec("docker ps --filter 'name=test_container' --format '{{.ID}}'", $data, $resultCode);
+        if(!empty($data)){
+            $this->tester->runShellCommand("docker rm  $(docker ps --filter 'name={$containerPrefix}' --format '{{.ID}}' -a) -f", false);
+        }
+        $data = [];
+        exec("docker images 'test_image*' --format '{{.ID}}'", $data, $resultCode);
+        if(!empty($data)){
+            $this->tester->runShellCommand("docker rmi $(docker images '{$imagePrefix}*' --format '{{.ID}}') -f", false);
+        }
+
         $newDir = self::DATA_DIR . self::UNPACKED . '/' . $id;
-        $service->build($newDir.'/'.self::MICRO_SERVICE_1_DOCKERFILE, $imageName, $containerName);
+        $dockerFilePath = $newDir.'/'.self::MICRO_SERVICE_1_DOCKERFILE;
+
+        $buildCommand = new BuildCommand($dockerFilePath, $imageName);
+        $runCommand = new RunCommand($containerName, $imageName);
+
+        $collection = new CommandCollection();
+        $collection->addCommand($buildCommand);
+        $collection->addCommand($runCommand);
+        $service = new BuildService(new CommandRunner());
+
+        $service->build($collection);
 
         $this->tester->runShellCommand("docker inspect -f '{{.State.Running}}' $(docker ps --filter 'name=$containerName' --format '{{.ID}}')", false);
         $this->tester->seeInShellOutput('true'); ///-- DZIAŁA!!!
