8504a793497b5c54d16aea89fd8914c28703b19a
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-04-26 14:06:02 +0200

    Repo Refactor

--- tests/unit/App/AppCore/Domain/Repository/uServiceRepositoryTest.php
+++ tests/unit/App/AppCore/Domain/Repository/uServiceRepositoryTest.php
@@ -18,36 +18,69 @@ class uServiceRepositoryTest extends \Codeception\Test\Unit
     {
     }
 
+//    // tests
+//    public function testSomeFeature()
+//    {
+//        // Given
+//        $id = 'id';
+//        $file = 'file';
+//        $movedToDir = 'movedToDir';
+//        $uServiceEntity = new uServiceEntity($movedToDir . $id, $file, $id);
+//        $persistGateway = $this->prophesize(PersistGatewayInterface::class);
+//        $persistGateway->nextId()->willReturn('id');
+//        $persistGateway->getAll()->willReturn([$uServiceEntity]);
+//        $persistGateway->persist($uServiceEntity)->shouldBeCalled()->willReturn($id);
+//        $persistGateway->find($id)->willReturn($uServiceEntity)->shouldBeCalled();
+//        $domain = new uService($file, $movedToDir);
+//
+//        $mapper = $this->prophesize(DomainEntityMapperInterface::class);
+//        $mapper->domain2Entity($id, $domain)->willReturn($uServiceEntity);
+//        $mapper->entity2Domain($uServiceEntity)->willReturn($domain);
+//        $repo = new uServiceRepository($persistGateway->reveal(), $mapper->reveal());  //jeśli repo zależy od mappera to będę mógł zrobić mocka do entity
+//
+//        // When
+//        $lastId = $repo->persist($domain, null);
+//
+//        // Then
+//        $this->tester->assertInstanceOf(uServiceInterface::class, $repo->find($lastId));
+//        $this->tester->assertNotEmpty($lastId);
+//        $this->tester->assertEquals($movedToDir.$lastId, $repo->find($lastId)->movedToDir());
+//        $this->tester->assertEquals($file, $repo->find($lastId)->file());
+//
+//        $this->tester->assertEquals($domain, $repo->find($lastId));
+//
+//    }
+
     // tests
-    public function testSomeFeature()
+    public function testSomeFeatureWithNull()
     {
         // Given
-        $id = 'id';
+        $nextId = 'nextId';
         $file = 'file';
         $movedToDir = 'movedToDir';
-        $uServiceEntity = new uServiceEntity($id, $movedToDir . $id, $file);
-        $persistGateway = $this->prophesize(PersistGatewayInterface::class);
-        $persistGateway->nextId()->willReturn('id');
-        $persistGateway->getAll()->willReturn([$uServiceEntity]);
-        $persistGateway->persist($uServiceEntity)->shouldBeCalled()->willReturn($id);
-        $persistGateway->find($id)->willReturn($uServiceEntity)->shouldBeCalled();
         $domain = new uService($file, $movedToDir);
 
+        $uServiceEntity = new uServiceEntity($movedToDir, $file, null);
+        $persistGateway = $this->prophesize(PersistGatewayInterface::class);
+        $persistGateway->nextId()->willReturn('nextId');
+        $persistGateway->persist($uServiceEntity)->shouldBeCalled();
+        $persistGateway->find($nextId)->willReturn($uServiceEntity);
+
+
         $mapper = $this->prophesize(DomainEntityMapperInterface::class);
-        $mapper->domain2Entity($id, $domain)->willReturn($uServiceEntity);
+        $mapper->domain2Entity(null, $domain)->willReturn($uServiceEntity);
         $mapper->entity2Domain($uServiceEntity)->willReturn($domain);
         $repo = new uServiceRepository($persistGateway->reveal(), $mapper->reveal());  //jeśli repo zależy od mappera to będę mógł zrobić mocka do entity
 
         // When
-        $lastId = $repo->persist($domain);
+        $repo->persist($domain, null);
 
         // Then
-        $this->tester->assertInstanceOf(uServiceInterface::class, $repo->find($lastId));
-        $this->tester->assertNotEmpty($lastId);
-        $this->tester->assertEquals($movedToDir.$lastId, $repo->find($lastId)->movedToDir());
-        $this->tester->assertEquals($file, $repo->find($lastId)->file());
+        $this->tester->assertInstanceOf(uServiceInterface::class, $repo->find($nextId));
+        $this->tester->assertEquals($movedToDir, $repo->find($nextId)->movedToDir());
+        $this->tester->assertEquals($file, $repo->find($nextId)->file());
 
-        $this->tester->assertEquals($domain, $repo->find($lastId));
+        $this->tester->assertEquals($domain, $repo->find($nextId));
 
     }
 }
