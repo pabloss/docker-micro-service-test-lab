744d433cb5c50dec36a7227297de9d223163f8d2
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-05-16 16:45:17 +0200

    deploy cest

--- tests/unit/App/Framework/Entity/UServiceTest.php
+++ tests/unit/App/Framework/Entity/UServiceTest.php
@@ -18,16 +18,19 @@ class UServiceTest extends \Codeception\Test\Unit
         $id = 1;
         $file = 'test';
         $dir = 'test_dir';
+        $unpacked = 'unpacked';
 
         $domainEntity = $this->prophesize(uServiceEntity::class);
         $domainEntity->id()->willReturn($id);
         $domainEntity->file()->willReturn($file);
         $domainEntity->movedToDir()->willReturn($dir);
+        $domainEntity->unpacked()->willReturn($unpacked);
 
-        $entity = UService::fromDomainEntity($domainEntity->reveal());
+        $entity = UService::fromDomainEntity($domainEntity->reveal(), null);
 
         $this->tester->assertEquals($id, $entity->getId());
         $this->tester->assertEquals($file, $entity->getFile());
         $this->tester->assertEquals($dir, $entity->getMovedToDir());
+        $this->tester->assertEquals($unpacked, $entity->getUnpacked());
     }
 }
---
+++ tests/unit/App/Framework/Service/Command/CommandFactoryTest.php
@@ -0,0 +1,50 @@
+<?php namespace App\Framework\Service\Command;
+
+use App\AppCore\Domain\Service\CommandFactoryInterface;
+use App\Framework\Service\Command\CommandFactory;
+
+class CommandFactoryTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $factory = new CommandFactory();
+
+        $buildCommand = new BuildCommand('Dockerfile', 'imagePref');
+        $runCommand = new RunCommand('containerPref', 'imagePref');
+
+        $this->tester->assertEquals(
+            $buildCommand,
+            $factory->createCommand('build', 'Dockerfile', 'imagePref')
+        );
+
+        $this->tester->assertEquals(
+            $runCommand,
+            $factory->createCommand('run', 'containerPref', 'imagePref')
+        );
+
+        $commandCollection = new CommandCollection();
+        $commandCollection->addCommand($buildCommand);
+        $commandCollection->addCommand($runCommand);
+        $this->tester->assertEquals(
+            $commandCollection,
+            $factory->createCollection([$buildCommand, $runCommand])
+        );
+
+        $this->tester->assertInstanceOf(CommandFactoryInterface::class, $factory);
+
+    }
+}
