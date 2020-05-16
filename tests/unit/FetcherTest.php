f41e15a501bc268341eb216f84c50011f87ef02e
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2020-05-16 16:51:46 +0200

    deploy cest

---
+++ tests/unit/FetcherTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\AppCore\Domain\Service\OutPutInterface;
+use App\Framework\Service\Command\Fetcher\Fetcher;
+use App\Framework\Service\WebSockets\Context\WrappedContext;
+
+class FetcherTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+        $wrappedContext = $this->prophesize(WrappedContext::class);
+        $wrappedContext->send(['test'])->shouldBeCalled();
+
+        $outputAdapter = $this->prophesize(OutPutInterface::class);
+        $outputAdapter->willBeConstructedWith([$wrappedContext->reveal()]);
+        $outputAdapter->writeln('test')->will(function ($args)use($wrappedContext){
+            $wrappedContext->reveal()->send([$args[0]]);
+        })->shouldBeCalled();
+
+
+        $fetcher = new Fetcher();
+        $fetcher->exec('echo -n test', $outputAdapter->reveal());
+    }
+}
